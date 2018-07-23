de40298945d0f1dd58c4807ce54fe46094fe9a09
Daekwang Ryu <dkdk.ryu@samsung.com>
2018-07-23 14:44:16 +0900

    [Tizen] Remove functions which using internal apis
    This reverts commit d38d27325e936bc8281b4119becac99fde91bddd.
    
    Change-Id: I23c313a246b80b29e6df020a422f16d598c43c4a

--- dali-csharp-binder/src/input-method-options.cpp
+++ dali-csharp-binder/src/input-method-options.cpp
@@ -157,53 +157,6 @@ SWIGEXPORT void SWIGSTDCALL CSharp_Dali_InputMethodOptions_RetrieveProperty(void
 }
 
 
-SWIGEXPORT unsigned int SWIGSTDCALL CSharp_Dali_InputMethodOptions_CompareAndSet(void * jarg1, int jarg2, void * jarg3, void * jarg4) {
-  unsigned int jresult ;
-  Dali::InputMethodOptions *arg1 = (Dali::InputMethodOptions *) 0 ;
-  Dali::InputMethod::Category::Type arg2 ;
-  Dali::InputMethodOptions *arg3 = 0 ;
-  int *arg4 = 0 ;
-  bool result;
-
-  arg1 = (Dali::InputMethodOptions *)jarg1;
-  arg2 = (Dali::InputMethod::Category::Type)jarg2;
-  arg3 = (Dali::InputMethodOptions *)jarg3;
-  if (!arg3) {
-    SWIG_CSharpSetPendingExceptionArgument(SWIG_CSharpArgumentNullException, "Dali::InputMethodOptions const & type is null", 0);
-    return 0;
-  }
-  arg4 = (int *)jarg4;
-  if (!arg4) {
-    SWIG_CSharpSetPendingExceptionArgument(SWIG_CSharpArgumentNullException, "int & type is null", 0);
-    return 0;
-  }
-  {
-    try {
-      result = (bool)(arg1)->CompareAndSet(arg2,(Dali::InputMethodOptions const &)*arg3,*arg4);
-    } catch (std::out_of_range& e) {
-      {
-        SWIG_CSharpException(SWIG_IndexError, const_cast<char*>(e.what())); return 0;
-      };
-    } catch (std::exception& e) {
-      {
-        SWIG_CSharpException(SWIG_RuntimeError, const_cast<char*>(e.what())); return 0;
-      };
-    } catch (Dali::DaliException e) {
-      {
-        SWIG_CSharpException(SWIG_UnknownError, e.condition); return 0;
-      };
-    } catch (...) {
-      {
-        SWIG_CSharpException(SWIG_UnknownError, "unknown error"); return 0;
-      };
-    }
-  }
-
-  jresult = result;
-  return jresult;
-}
-
-
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_delete_InputMethodOptions(void * jarg1) {
   Dali::InputMethodOptions *arg1 = (Dali::InputMethodOptions *) 0 ;
 
