66d713d7e57d0317cc7516c36c76eb68de9e0862
dongsug.song <dongsug.song@samsung.com>
2018-10-01 13:37:01 +0900

    Revert "[Tizen][Temporary Fix] Restore to call InputMethodContext::Finalize()"
    This reverts commit b097276829c6e919961845e76cb135dd0afa28b4.
    
    Change-Id: I092139542d396716bdb170ef637538b98669e556

--- dali-csharp-binder/src/input-method-context.cpp
+++ dali-csharp-binder/src/input-method-context.cpp
@@ -460,12 +460,14 @@ SWIGEXPORT void SWIGSTDCALL CSharp_Dali_delete_InputMethodContext_CallbackData(v
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_InputMethodContext_Finalize(void * jarg1) {
-  Dali::InputMethodContext *arg1 = (Dali::InputMethodContext *) 0 ;
+  //This is not necessary in tizen5.0, but because of backwards compatibility, the method is maintained but the body is deleted.
+  //Dali::InputMethodContext *arg1 = (Dali::InputMethodContext *) 0 ;
 
-  arg1 = (Dali::InputMethodContext *)jarg1;
+  //arg1 = (Dali::InputMethodContext *)jarg1;
   {
     try {
-      (arg1)->Finalize();
+      //This is not necessary in tizen5.0, but because of backwards compatibility, the method is maintained but the body is deleted.
+      //(arg1)->Finalize();
     } catch (std::out_of_range& e) {
       {
         SWIG_CSharpException(SWIG_IndexError, const_cast<char*>(e.what())); return ;
