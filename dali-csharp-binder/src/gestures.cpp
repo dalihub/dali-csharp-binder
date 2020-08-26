8e8ece14d5300560c87f1efb2fbe57135125d80a
Adeel Kazmi <adeel.kazmi@samsung.com>
2020-08-26 20:47:15 +0100

    Changes after RotationGesture made public
    Change-Id: I593bab7c21785d54b0d0a503b8de86375862754f

--- dali-csharp-binder/src/gestures.cpp
+++ dali-csharp-binder/src/gestures.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,8 @@
 #endif
 
 #include "common.h"
-#include <dali/devel-api/events/gesture-devel.h>
-#include <dali/devel-api/events/rotation-gesture.h>
-#include <dali/devel-api/events/rotation-gesture-detector.h>
+#include <dali/public-api/events/rotation-gesture.h>
+#include <dali/public-api/events/rotation-gesture-detector.h>
 
 #ifdef __cplusplus
 extern "C" {
