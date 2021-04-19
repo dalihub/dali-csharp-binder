29c9431cfecd8fa7b47060ef1dc31aee74146c14
Joogab Yun <joogab.yun@samsung.com>
2021-04-19 16:51:13 +0900

    Revert "Change TOUCH_AREA to TOUCH_AREA_OFFSET"
    Change-Id: I60e45206c5e20a395322f11b9bb761bb0fc05b95

--- dali-csharp-binder/src/devel-property-wrap.cpp
+++ dali-csharp-binder/src/devel-property-wrap.cpp
@@ -69,9 +69,9 @@ SWIGEXPORT int SWIGSTDCALL CSharp_Actor_Property_CAPTURE_ALL_TOUCH_AFTER_START_g
   return Dali::DevelActor::Property::CAPTURE_ALL_TOUCH_AFTER_START;
 }
 
-SWIGEXPORT int SWIGSTDCALL CSharp_Actor_Property_TOUCH_AREA_OFFSET_get() {
+SWIGEXPORT int SWIGSTDCALL CSharp_Actor_Property_TOUCH_AREA_get() {
 
-  return Dali::DevelActor::Property::TOUCH_AREA_OFFSET;
+  return Dali::DevelActor::Property::TOUCH_AREA;
 }
 
 SWIGEXPORT int SWIGSTDCALL CSharp_Actor_Property_BLEND_EQUATION_get() {
