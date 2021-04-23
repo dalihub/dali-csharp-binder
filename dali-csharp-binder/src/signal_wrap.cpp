208bb5d7c61c62179d4275fe1a660fe116aff68f
Bartlomiej Grzelewski <b.grzelewski@samsung.com>
2021-04-23 15:08:20 +0200

    [Tizen][AT-SPI] Bindings for Bridge::{Enable,Disable}AutoInit() and etc.
    This includes followings
    [ATSPI] Fix StringToVoid::GetResult function
    [AT-SPI] Remove deprecated ReadingInfoTypes bindings
    [NUI][AT-SPI] Simplify implementation of States
    [AT-SPI] Add Dali::AtspiAccessibility::SuppressScreenReader()
    [AT-SPI] Bindings for Bridge::{Enable,Disable}AutoInit()
    
    Change-Id: I64f36171a02e770934a93c8a0f601d3ec16f1d90

--- dali-csharp-binder/src/signal_wrap.cpp
+++ dali-csharp-binder/src/signal_wrap.cpp
@@ -53,17 +53,6 @@ SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_Disconnect(void *arg
     });
 }
 
-// TODO - DEPRECATED - REMOVE AFTER TIZENFX MERGE
-SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_Emit(void *arg1, char *arg2) {
-    GUARD_ON_NULL_RET(arg1);
-    GUARD_ON_NULL_RET(arg2);
-    try_catch([&]() {
-        auto object = (StringToVoidSignal*)arg1;
-        std::string arg2_str(arg2);
-        object->Emit(arg2_str);
-    });
-}
-
 SWIGEXPORT void* SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_new() {
     try_catch([&]() {
         return new StringToVoidSignal();
@@ -79,23 +68,10 @@ SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_delete(void *arg1) {
     });
 }
 
-// TODO - DEPRECATED - REMOVE AFTER TIZENFX MERGE
-SWIGEXPORT char * SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_Convert_Param1(void *arg1) {
-    GUARD_ON_NULL_RET0(arg1);
-    try_catch([&]() {
-        auto result = (std::string*)arg1;
-        return SWIG_csharp_string_callback(result->c_str());
-    });
-    return nullptr;
-}
-
 SWIGEXPORT char * SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_GetResult(void *arg1) {
     GUARD_ON_NULL_RET0(arg1);
-    try_catch([&]() {
-        auto result = (std::string*)arg1;
-        return SWIG_csharp_string_callback(result->c_str());
-    });
-    return nullptr;
+    std::string *data = (std::string*)arg1;
+    return SWIG_csharp_string_callback(data->c_str());
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_SetResult(void *arg1, char *arg2) {
@@ -206,4 +182,3 @@ SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_SetResult(void 
 #ifdef __cplusplus
 }
 #endif
-
