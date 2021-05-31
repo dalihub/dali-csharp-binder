8eda290c87a36697abc5b53dedb3fba3f3187017
Seoyeon Kim <seoyeon2.kim@samsung.com>
2021-05-31 10:19:35 +0900

    [AT-SPI] Change the type of GesturePair Set/Get binding
    - The second value of GesturePair should be bool type, just same as dali.
     Changed the type and its binding.
    
    Change-Id: I643d3eb492a1de0a86e6cdce0eaf9857b9c71b60
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- dali-csharp-binder/src/signal_wrap.cpp
+++ dali-csharp-binder/src/signal_wrap.cpp
@@ -14,73 +14,73 @@ using StringToVoidFunc = void (*)(std::string &);
 using StringToVoidSignal = Dali::Signal<void(std::string &)>;
 
 SWIGEXPORT unsigned int SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_Empty(void *arg1) {
-    bool result = false;
-    GUARD_ON_NULL_RET0(arg1);
-    try_catch([&]() {
-        auto object = (StringToVoidSignal*)arg1;
-        result = object->Empty();
-    });
-    return result;
+  bool result = false;
+  GUARD_ON_NULL_RET0(arg1);
+  try_catch([&]() {
+      auto object = (StringToVoidSignal*)arg1;
+      result = object->Empty();
+  });
+  return result;
 }
 
 SWIGEXPORT unsigned long SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_GetConnectionCount(void *arg1) {
-    std::size_t result = 0;
-    GUARD_ON_NULL_RET0(arg1);
-    try_catch([&]() {
-        auto object = (StringToVoidSignal*)arg1;
-        result = object->GetConnectionCount();
-    });
-    return result;
+  std::size_t result = 0;
+  GUARD_ON_NULL_RET0(arg1);
+  try_catch([&]() {
+      auto object = (StringToVoidSignal*)arg1;
+      result = object->GetConnectionCount();
+  });
+  return result;
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_Connect(void *arg1, void *arg2) {
-    GUARD_ON_NULL_RET(arg1);
-    GUARD_ON_NULL_RET(arg2);
-    try_catch([&]() {
-        auto object = (StringToVoidSignal*)arg1;
-        auto func = (StringToVoidFunc)arg2;
-        object->Connect(func);
-    });
+  GUARD_ON_NULL_RET(arg1);
+  GUARD_ON_NULL_RET(arg2);
+  try_catch([&]() {
+      auto object = (StringToVoidSignal*)arg1;
+      auto func = (StringToVoidFunc)arg2;
+      object->Connect(func);
+  });
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_Disconnect(void *arg1, void *arg2) {
-    GUARD_ON_NULL_RET(arg1);
-    GUARD_ON_NULL_RET(arg2);
-    try_catch([&] () {
-        auto object = (StringToVoidSignal*)arg1;
-        auto func = (StringToVoidFunc)arg2;
-        object->Disconnect(func);
-    });
+  GUARD_ON_NULL_RET(arg1);
+  GUARD_ON_NULL_RET(arg2);
+  try_catch([&] () {
+      auto object = (StringToVoidSignal*)arg1;
+      auto func = (StringToVoidFunc)arg2;
+      object->Disconnect(func);
+  });
 }
 
 SWIGEXPORT void* SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_new() {
-    try_catch([&]() {
-        return new StringToVoidSignal();
-    });
-    return nullptr;
+  try_catch([&]() {
+      return new StringToVoidSignal();
+  });
+  return nullptr;
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_delete(void *arg1) {
-    GUARD_ON_NULL_RET(arg1);
-    try_catch([&]() {
-        auto object = (StringToVoidSignal*)arg1;
-        delete object;
-    });
+  GUARD_ON_NULL_RET(arg1);
+  try_catch([&]() {
+      auto object = (StringToVoidSignal*)arg1;
+      delete object;
+  });
 }
 
 SWIGEXPORT char * SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_GetResult(void *arg1) {
-    GUARD_ON_NULL_RET0(arg1);
-    std::string *data = (std::string*)arg1;
-    return SWIG_csharp_string_callback(data->c_str());
+  GUARD_ON_NULL_RET0(arg1);
+  std::string *data = (std::string*)arg1;
+  return SWIG_csharp_string_callback(data->c_str());
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_StringToVoid_SetResult(void *arg1, char *arg2) {
-    GUARD_ON_NULL_RET(arg1);
-    GUARD_ON_NULL_RET(arg2);
-    try_catch([&]() {
-        auto result = (std::string*)arg1;
-        (*result) = arg2;
-    });
+  GUARD_ON_NULL_RET(arg1);
+  GUARD_ON_NULL_RET(arg2);
+  try_catch([&]() {
+      auto result = (std::string*)arg1;
+      (*result) = arg2;
+  });
 }
 
 /*
@@ -93,90 +93,91 @@ using GesturePairFunc = void (*)(GesturePair &);
 using GesturePairSignal = Dali::Signal<void(GesturePair&)>;
 
 SWIGEXPORT unsigned int SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_Empty(void *arg1) {
-    bool result = false;
-    GUARD_ON_NULL_RET0(arg1);
-    try_catch([&]() {
-        auto object = (GesturePairSignal*)arg1;
-        result = object->Empty();
-    });
-    return result;
+  bool result = false;
+  GUARD_ON_NULL_RET0(arg1);
+  try_catch([&]() {
+      auto object = (GesturePairSignal*)arg1;
+      result = object->Empty();
+  });
+  return result;
 }
 
 SWIGEXPORT unsigned long SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_GetConnectionCount(void *arg1) {
-    std::size_t result = 0;
-    GUARD_ON_NULL_RET0(arg1);
-    try_catch([&]() {
-        auto object = (GesturePairSignal*)arg1;
-        result = object->GetConnectionCount();
-    });
-    return result;
+  std::size_t result = 0;
+  GUARD_ON_NULL_RET0(arg1);
+  try_catch([&]() {
+      auto object = (GesturePairSignal*)arg1;
+      result = object->GetConnectionCount();
+  });
+  return result;
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_Connect(void *arg1, void *arg2) {
-    GUARD_ON_NULL_RET(arg1);
-    GUARD_ON_NULL_RET(arg2);
-    try_catch([&]() {
-        auto object = (GesturePairSignal*)arg1;
-        auto func = (GesturePairFunc)arg2;
-        object->Connect( func );
-    });
+  GUARD_ON_NULL_RET(arg1);
+  GUARD_ON_NULL_RET(arg2);
+  try_catch([&]() {
+      auto object = (GesturePairSignal*)arg1;
+      auto func = (GesturePairFunc)arg2;
+      object->Connect( func );
+  });
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_Disconnect(void *arg1, void *arg2) {
-    GUARD_ON_NULL_RET(arg1);
-    GUARD_ON_NULL_RET(arg2);
-    try_catch([&] () {
-        auto object = (GesturePairSignal*)arg1;
-        auto func = (GesturePairFunc)arg2;
-        object->Disconnect(func);
-    });
+  GUARD_ON_NULL_RET(arg1);
+  GUARD_ON_NULL_RET(arg2);
+  try_catch([&] () {
+      auto object = (GesturePairSignal*)arg1;
+      auto func = (GesturePairFunc)arg2;
+      object->Disconnect(func);
+  });
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_Emit(void *arg1, void *arg2) {
-    GUARD_ON_NULL_RET(arg1);
-    GUARD_ON_NULL_RET(arg2);
-    try_catch([&]() {
-        auto object = (GesturePairSignal*)arg1;
-        auto gesturePair = (GesturePair*)arg2;
-        object->Emit(*gesturePair);
-    });
+  GUARD_ON_NULL_RET(arg1);
+  GUARD_ON_NULL_RET(arg2);
+  try_catch([&]() {
+      auto object = (GesturePairSignal*)arg1;
+      auto gesturePair = (GesturePair*)arg2;
+      object->Emit(*gesturePair);
+  });
 }
 
 SWIGEXPORT void* SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_new() {
-    try_catch([&]() {
-        return new GesturePairSignal();
-    });
-    return nullptr;
+  try_catch([&]() {
+      return new GesturePairSignal();
+  });
+  return nullptr;
 }
 
 SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_delete(void *arg1) {
-    GUARD_ON_NULL_RET(arg1);
-    try_catch([&]() {
-        auto object = (GesturePairSignal*)arg1;
-        delete object;
-    });
+  GUARD_ON_NULL_RET(arg1);
+  try_catch([&]() {
+      auto object = (GesturePairSignal*)arg1;
+      delete object;
+  });
 }
 
 SWIGEXPORT unsigned int SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_GetSizeOfGestureInfo(void) {
-    return sizeof(Dali::Accessibility::GestureInfo);
+  return sizeof(Dali::Accessibility::GestureInfo);
 }
 
-SWIGEXPORT int SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_GetResult(void *arg1) {
-    int result = 0;
-    GUARD_ON_NULL_RET0(arg1);
-    try_catch([&]() {
-        auto gp = (GesturePair*)arg1;
-        result = gp->second;
-    });
-    return result;
+SWIGEXPORT bool SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_GetResult(void *arg1) {
+  bool result = false;
+  GUARD_ON_NULL_RET0(arg1);
+  try_catch([&]() {
+      auto gesturePair = (GesturePair*)arg1;
+      result = (bool)gesturePair->second;
+  });
+  return result;
 }
 
-SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_SetResult(void *arg1, int result) {
-    GUARD_ON_NULL_RET(arg1);
-    try_catch([&]() {
-        auto gp = (GesturePair*)arg1;
-        gp->second = (bool)result;
-    });
+SWIGEXPORT void SWIGSTDCALL CSharp_Dali_Signal_GesturePairToVoid_SetResult(void *arg1, bool arg2) {
+  bool result = arg2;
+  GUARD_ON_NULL_RET(arg1);
+  try_catch([&]() {
+      auto gesturePair = (GesturePair*)arg1;
+      gesturePair->second = result;
+  });
 }
 
 #ifdef __cplusplus
