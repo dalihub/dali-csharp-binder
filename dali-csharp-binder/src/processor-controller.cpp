0f6e62b353a9a60a211f51ff47e62ff3a4ccc463
Heeyong Song <heeyong.song@samsung.com>
2021-07-26 17:58:02 +0900

    Revert "[Tizen] fix coverity issue to make processor-controller catch dali exception"
    This reverts commit df12b0a267f42e979039a04c5ebd56ea0a3145d1.
    
    Change-Id: Ic1757a0ee91b2057281f2aedeb85a1a394ea6ea3

--- dali-csharp-binder/src/processor-controller.cpp
+++ dali-csharp-binder/src/processor-controller.cpp
@@ -22,49 +22,16 @@
 extern "C" {
 #endif
 
-#define CALL_CATCH_EXCEPTION(ret)                                          \
-  catch (std::out_of_range & e)                                            \
-  {                                                                        \
-    SWIG_CSharpException(SWIG_IndexError, const_cast<char *>(e.what()));   \
-    return ret;                                                            \
-  }                                                                        \
-  catch (std::exception & e)                                               \
-  {                                                                        \
-    SWIG_CSharpException(SWIG_RuntimeError, const_cast<char *>(e.what())); \
-    return ret;                                                            \
-  }                                                                        \
-  catch (Dali::DaliException e)                                            \
-  {                                                                        \
-    SWIG_CSharpException(SWIG_UnknownError, e.condition);                  \
-    return ret;                                                            \
-  }                                                                        \
-  catch (...)                                                              \
-  {                                                                        \
-    SWIG_CSharpException(SWIG_UnknownError, "unknown error");              \
-    return ret;                                                            \
-  }
-
 ProcessorController::ProcessorController()
 : mHandler(nullptr)
 {
-  {
-    try
-    {
-      Dali::Adaptor::Get().RegisterProcessor(*this);
-    }
-    CALL_CATCH_EXCEPTION();
-  }
+  Dali::Adaptor::Get().RegisterProcessor(*this);
 }
+
 
 ProcessorController::~ProcessorController()
 {
-  {
-    try
-    {
-      Dali::Adaptor::Get().UnregisterProcessor(*this);
-    }
-    CALL_CATCH_EXCEPTION();
-  }
+  Dali::Adaptor::Get().UnregisterProcessor(*this);
 }
 
 void ProcessorController::Process(bool postProcessor)
