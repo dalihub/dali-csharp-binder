f2b449ddde34dce4d8886326292f5f815f842bd2
Jaehyun Cho <jae_hyun.cho@samsung.com>
2024-08-27 15:45:59 +0900

    [Tizen] Fix VOA crash issue
    This reverts commit 5941970d11f20b2c913130ca4114df06bdedfe9a.

--- dali-csharp-binder/widget-viewer-dali/widget-view-wrap.cpp
+++ dali-csharp-binder/widget-viewer-dali/widget-view-wrap.cpp
@@ -42,6 +42,50 @@ extern "C" {
 #endif
 
 
+SWIGEXPORT Dali::Toolkit::Control * SWIGSTDCALL CSharp_Dali_WidgetView_SWIGUpcast(Dali::WidgetView::WidgetView *jarg1) {
+    return (Dali::Toolkit::Control *)jarg1;
+}
+
+SWIGEXPORT void * SWIGSTDCALL CSharp_Dali_new_WidgetView_Property() {
+  void * jresult ;
+  Dali::WidgetView::WidgetView::Property *result = 0 ;
+
+  {
+    try {
+      result = (Dali::WidgetView::WidgetView::Property *)new Dali::WidgetView::WidgetView::Property();
+    } CALL_CATCH_EXCEPTION(0);
+  }
+
+  jresult = (void *)result;
+  return jresult;
+}
+
+SWIGEXPORT void SWIGSTDCALL CSharp_Dali_delete_WidgetView_Property(void * jarg1) {
+  Dali::WidgetView::WidgetView::Property *arg1 = (Dali::WidgetView::WidgetView::Property *) 0 ;
+
+  arg1 = (Dali::WidgetView::WidgetView::Property *)jarg1;
+  {
+    try {
+      delete arg1;
+    } CALL_CATCH_EXCEPTION();
+  }
+
+}
+
+SWIGEXPORT void * SWIGSTDCALL CSharp_Dali_new_WidgetView__SWIG_0() {
+  void * jresult ;
+  Dali::WidgetView::WidgetView *result = 0 ;
+
+  {
+    try {
+      result = (Dali::WidgetView::WidgetView *)new Dali::WidgetView::WidgetView();
+    } CALL_CATCH_EXCEPTION(0);
+  }
+
+  jresult = (void *)result;
+  return jresult;
+}
+
 SWIGEXPORT int SWIGSTDCALL CSharp_Dali_WidgetView_Property_WIDGET_ID_get() {
   int jresult ;
   int result;
