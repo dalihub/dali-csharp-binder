fa70e2cf8ba6cae9a76b7f46843fd441c0955561
Bowon Ryu <bowon.ryu@samsung.com>
2026-03-09 11:29:13 +0900

    Bind deferred relayout to text editor
    https://review.tizen.org/gerrit/c/platform/core/uifw/dali-toolkit/+/339655
    
    Change-Id: I53a40729e85481063af3c8109890ed64266e4677
    Signed-off-by: Bowon Ryu <bowon.ryu@samsung.com>

--- dali-csharp-binder/dali-toolkit/text-editor-wrap.cpp
+++ dali-csharp-binder/dali-toolkit/text-editor-wrap.cpp
@@ -1413,6 +1413,11 @@ SWIGEXPORT int SWIGSTDCALL CSharp_Dali_TextEditor_Property_ENABLE_CURSOR_INSET_g
   return (int)Dali::Toolkit::DevelTextEditor::Property::ENABLE_CURSOR_INSET;
 }
 
+SWIGEXPORT int SWIGSTDCALL CSharp_Dali_TextEditor_Property_ENABLE_DEFERRED_RELAYOUT_get()
+{
+  return (int)Dali::Toolkit::DevelTextEditor::Property::ENABLE_DEFERRED_RELAYOUT;
+}
+
 SWIGEXPORT int32_t SWIGSTDCALL CSharp_Dali_TextEditor_RegisterFontVariationProperty(void* pTextEditor, char* pTag)
 {
   Dali::Toolkit::TextEditor* textEditor = (Dali::Toolkit::TextEditor*)0;
